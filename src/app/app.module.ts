11ead6ea34e721a364bd784821c8eefdf0bfa820
Csaba Todor <csaba.todor@outlook.com>
2017-10-30 11:50:59 +0200

    Imported Modules in App Mo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from "@angular/common/http";
 
+// Modules
+import { AppRoutingModule } from "./app-routing.module";
+
+// Components
 import { AppComponent } from './app.component';
+import { HomeComponent } from "./home/home.component";
+import { AppInfoComponent } from "./app-info/app-info.component";
+import { HeaderComponent } from "./shared/header/header.component";
+import { FooterComponent } from "./shared/footer/footer.component";
+import { Error404Component } from "./error-404/error-404.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AppInfoComponent,
+    HeaderComponent,
+    FooterComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule.withServerTransition({appId: 'MyGitRepos-with-AngularUniversal'}),
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
