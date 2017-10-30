97b8d4216df15226bfaaeb04a22576810520bfc5
Csaba Todor <csaba.todor@outlook.com>
2017-10-30 11:49:17 +0200

    Revamped AppComponent, imported

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs/Observable";
-import { REPOS_LIST } from "./constants";
 
 @Component({
   selector: 'app-root',
@@ -10,16 +7,4 @@ import { REPOS_LIST } from "./constants";
 })
 export class AppComponent {
 
-  repos: Observable<any>;
-  hrepos = [
-    { name: 'Static Data One'},
-    { name: 'Static Data Two'},
-    { name: 'Static Data Three'},
-    { name: 'Static Data Four'},
-    { name: 'Static Data Five'}
-  ]
-
-  constructor(private http: HttpClient) {
-    this.repos = this.http.get(REPOS_LIST);
-  }
 }
